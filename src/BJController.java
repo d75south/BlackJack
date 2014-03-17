edf633964b85f06f38727e9422651bbf89fcbe4a
greenchuck <greenchuck@gmail.com>
2014-03-16 22:34:32 -0400

    BJ Controller--added CardStqck/dealer logic/do while loop to iterate games/disallowed game after bank=0 Hand--added soft 11 logic BJDriver--removed CardStack

--- src/BJController.java
+++ src/BJController.java
@@ -4,24 +4,31 @@
  * -->iterate after a hand is done---do you want to continue?
  */
 
+
+
 import java.util.Scanner;
 
 public class BJController {
 private int bank;	//does this need to be private? 
-int bet, status, hit;		//status 1=playing 2=win 3=loss 4=push
+int bet, status, hit, again;		//status 1=playing 2=win 3=loss 4=push
 
 
 	public BJController()
 	{
 		bank=200;	//set the initial bank level that a player receives at the beginning of the game
-			
 		Scanner scan=new Scanner(System.in);
+		do
+		{	
+		CardStack cs = new CardStack(); /* This instance creates and initializes the static cardStack hash map that the
+		   static deal method operates on */	
 		System.out.println("What is your bet?");
 		bet=scan.nextInt();
 		hit=0;
 		
+		
 		Hand p1=new Hand("Player 1");
-		Hand p2=new Hand("Dealer");		
+		Hand p2=new Hand("Dealer");
+		
 		
 		//Initial Deal===>Could turn in this into a for loop
 		p2.deal();
@@ -40,24 +47,20 @@ int bet, status, hit;		//status 1=playing 2=win 3=loss 4=push
 			if (p1.getScore()!=21)	//checks player score for blackjack if dealer does not have blackjack
 			{
 				System.out.println (p1.getPlayerName()+"     Score: "+p1.getScore());
-				System.out.println(p1.getHand());
-				System.out.println(p1.getHandName());
+				System.out.println(p1.getHandName()+" "+p1.getHand());
 				System.out.println(p2.getPlayerName());
-				System.out.println("["+p2.getDealer()+"]");
-				System.out.println("["+p2.getDealerHandName()+"]");
+				System.out.println(p2.getDealerHandName()+" ["+p2.getDealer()+"]");
 				System.out.println("Do you want to hit (1) or stand (0)?");
 					hit=scan.nextInt();
-				
-				while (hit!=0)	//player chooses Hit
+					
+				while (hit==1)	//player chooses Hit
 				{
 					p1.deal();
 					p1.scoreCalc();
 					System.out.println (p1.getPlayerName()+"     Score: "+p1.getScore());
-					System.out.println(p1.getHand());
-					System.out.println(p1.getHandName());
+					System.out.println(p1.getHandName()+" "+p1.getHand());
 					System.out.println(p2.getPlayerName());
-					System.out.println("["+p2.getDealer()+"]");
-					System.out.println("["+p2.getDealerHandName()+"]");
+					System.out.println(p2.getDealerHandName()+" ["+p2.getDealer()+"]");
 					if (p1.getScore()<=21)	//if player hasnt busted, they can continue to hit
 					{
 						System.out.println("Do you want to hit (1) or stand (0)?");
@@ -73,15 +76,29 @@ int bet, status, hit;		//status 1=playing 2=win 3=loss 4=push
 					System.out.println(p1.getScore()+", You bust, and lose: "+bet+" bitcoins.\nBankroll:"+bank);	
 					
 				}
-				else	 //player doesn't bust, player stands, compare and score hands
+				else	 //player doesn't bust, player stands, dealer plays, compare and score hands
 				{
+					//Dealer Plays
+					while (p2.getScore()<17)
+					{
+						p2.deal();
+						p2.scoreCalc();
+					}
+					if (p2.getScore()>21)
+					{
+						status=1;
+						bank+=bet;
+						System.out.println("Dealer busts "+p2.getScore());
+					}
+					else
+					{	
 					if (p2.getScore()>p1.getScore())	//dealer wins
 					{
 						status=3;
 						bank-=bet;
 						System.out.println("Dealer has"+p2.getScore()+", You lose: "+bet+" bitcoins.\nBankroll:"+bank);	
 					}
-					else
+					else		//derek do you want brackets on else statements like this--just calls an if statement
 						if (p2.getScore()<p1.getScore())	//player1 wins
 						{
 							status=2;
@@ -93,6 +110,7 @@ int bet, status, hit;		//status 1=playing 2=win 3=loss 4=push
 							status=3;
 							System.out.println("It's a push!");
 						}
+					}
 				}//end of hand comparison
 			}
 			else //player has blackjack, dealer doesn't
@@ -123,10 +141,25 @@ int bet, status, hit;		//status 1=playing 2=win 3=loss 4=push
 //		Printouts for Testing
 //***********************************************
 		System.out.println (p1.getPlayerName()+"     Score: "+p1.getScore());
-		System.out.println(p1.getHand());
-		System.out.println(p1.getHandName());
+		System.out.println(p1.getHandName()+" "+p1.getHand());
 		System.out.println (p2.getPlayerName()+"     Score: "+p2.getScore());
 		System.out.println(p2.getHand());
-		System.out.println(p2.getHandName());
-	}	
+		System.out.println(p2.getDealerHandName()+" "+p2.getHand());
+		System.out.println();
+		if (bank>0)
+		{
+			System.out.println("Do you want to play again? (1 if yes, 0 if no)?");
+			again=scan.nextInt();
+			System.out.println("again: "+again);
+		}
+		else 
+		{
+			System.out.println("You have exhausted your bankroll, better luck next time and thanks for playing.");
+			again=0;
+		}
+
+		
+		} ///end of Do statement
+		while (again==1);
+		}	
 }
--- src/Hand.java
+++ src/Hand.java
@@ -5,8 +5,10 @@ import java.util.Scanner;	//for testing only
 public class Hand {
 private String playerName;
 private final int MAX=10;
-public int score;
+public int score, soft=-1,scoreCalc=0;
 
+
+//ArrayList<Integer> handArray= new ArrayList<Integer>();
 List<Integer> handArray= new ArrayList<Integer>();
 List<String> cardNameArray = new ArrayList<String>();
 
@@ -17,27 +19,51 @@ Scanner scan=new Scanner(System.in);
 		playerName=player;
 	}
 	
-	//returns a card
+	//returns a card, eventually will call method from Dereks cardStack class
 	public void deal()
 	{
+		//handArray.add((int)(Math.random()*MAX)+2);	//for now just calculates an integer from 2-11					
+		//System.out.println("enter test card number "+handArray.size()+1+ " for "+playerName);
+		//handArray.add(scan.nextInt());
+		//if (handArray.get(handArray.size()-1)==11)
+		//	soft+=1;
 		List<String> methodList = new ArrayList<String>();		
 		methodList.addAll(CardStack.deal());		
 		
 		handArray.add(Integer.parseInt(methodList.get(1)));		
 		cardNameArray.add(methodList.get(0));
-		
-		//System.out.println("enter test card number "+handArray.size()+1+ " for "+playerName);
-		//handArray.add(scan.nextInt());
-		} 
+	}
 	
 	public void scoreCalc()
 	{
 		score=0;
+		
 		for (int num=0;num<handArray.size();num++)
-			{
-			score+=(int)handArray.get(num);
+		{	
+			if (handArray.get(num)==0){
+				handArray.remove(num);	//removes value at the index where last Ace was found
+				handArray.add(num,11);	
+				
+				soft=num;					//feeding the soft variable the index for the last Ace 
+											//in the array					
+				System.out.println(playerName+"Soft= "+soft);	//just for testing
+				System.out.println("Soft="+soft+" Hand: "+handArray.toString());	//just for testing
 			}
-	}
+			
+			score+=(int)handArray.get(num);	//add value of handArray to score
+
+			if (score>21&&soft>=0)
+			{
+				handArray.remove(soft);	//removes value at the index where last Ace was found
+				handArray.add(soft,1);	//replaces Ace value of 11 with 1
+				soft=-1;					//initializes soft value to -1
+				num=-1;					//initializes num to -1 so for loop starts at 0 again
+				score=0;					//initializes score to calculate from scratch at beginning of for loop
+				System.out.println("Soft 11 was changed to 1");	//just for testing
+				System.out.println("Soft="+soft+" Hand: "+handArray.toString());	//just for testing	
+			}	//end if score score>21&&soft>=0
+		}	//end for loop
+	}	//end ScoreCalc Method
 	
 	public int getScore()
 	{
@@ -51,23 +77,22 @@ Scanner scan=new Scanner(System.in);
 	{
 		return handArray.toString();
 	}
-
 	
 	public String getHandName()
 	{
 		return cardNameArray.toString();
 	}
 	
+	public String getDealerHandName()	//this just shows the first card in an array for testing
+	{
+		return cardNameArray.toString();
+		
+	}
+	
 	
 	public String getDealer()	//this just shows the first card in an array for testing
 	{
 		return handArray.get(0).toString();
-		
-	}
-	
-	public String getDealerHandName()	//this just shows the first card in an array for testing
-	{
-		return cardNameArray.get(0).toString();
 		
 	}
 	
